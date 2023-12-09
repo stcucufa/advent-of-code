133bc4a112235d08e0dff0d8ae40aca7aad54cd7
stcucufa <githubnyou@romulusetrem.us>
2023-12-09 08:46:27 -0500

    Part 2 (WIP)

--- 2023/08-2.ml
+++ 2023/08-2.ml
@@ -18,6 +18,8 @@ let split_head xs = match xs with
     | hd :: _ :: tl -> hd, tl
     | _ -> failwith "not enough content"
 
+let ends_with c string = String.length string - 1 |> String.get string |> phys_equal c
+
 let parse_node input =
     tokenize input
     |> split_head
@@ -25,18 +27,23 @@ let parse_node input =
             String.strip ~drop:(fun c -> phys_equal c '(' || phys_equal c ')' || phys_equal c ',')
         ) p |> to_pair)
 
-let follow instructions =
+let follow dirs graph pair = match pair with origin, dest ->
     let rec follow_step dirs graph k current =
-        if equal_string current "ZZZ" then k else
+        if equal_string current dest then k else
             match dirs with
             | [] -> failwith "no directions"
             | d :: ds -> let l, r = Map.find_exn graph current in
                 follow_step (ds @ [d]) graph (k + 1) (match d with 'L' -> l | 'R' -> r | _ -> failwith "?")
-    in match instructions with
-    dirs, graph -> follow_step dirs graph 0 "AAA"
+    in follow_step dirs graph 0 origin
 
-let result = In_channel.create "input-08.txt"
+let follow_all instructions =
+    match instructions with
+    dirs, graph -> let keys = Map.keys graph in
+        (*List.map ~f:(follow dirs graph)*) (List.cartesian_product
+            (List.filter ~f:(ends_with 'A') keys) (List.filter ~f:(ends_with 'Z') keys))
+
+let result = In_channel.create "test-input-08-3.txt"
     |> In_channel.input_lines
     |> split_head
     |> map_pair String.to_list (fun nodes -> List.map ~f:(parse_node) nodes |> Graph.of_alist_exn)
-    |> follow
+    |> follow_all

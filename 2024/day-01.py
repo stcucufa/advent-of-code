a85a442ea6262fce5be8b29fbc3848faf55bd90e
stcucufa <githubnyou@romulusetrem.us>
2024-12-01 14:48:18 +0900

    3R01 AOC 2024, day 1

---
+++ 2024/day-01.py
@@ -0,0 +1,31 @@
+import pathlib
+import re
+import sys
+
+def unzip(pairs):
+    xs = []
+    ys = []
+    for x, y in pairs:
+        xs.append(x)
+        ys.append(y)
+    return xs, ys
+
+def parse(puzzle_input):
+    return unzip([[int(n) for n in re.split(r"\s+", line)] for line in puzzle_input.split("\n")])
+
+def part1(data):
+    return sum(abs(x - y) for x, y in zip(*data))
+
+def part2(data):
+    xs, ys = data
+    return sum(sum(y for y in ys if y == x) for x in xs)
+
+def solve(puzzle_input):
+    data = parse(puzzle_input)
+    return part1(data), part2(data)
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        print("\n".join(str(solution) for solution in solve(puzzle_input)))

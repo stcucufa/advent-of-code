ce9a7219cd4e588e07ce0f27724db04f53ad69e4
stcucufa <githubnyou@romulusetrem.us>
2025-06-10 14:01:44 +0200

    4I08 AOC 2017, day 1
    In Swift.

---
+++ 2017/day-01.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func part1(_ digits: [Int]) {
+    let n = digits.count
+    var sum = 0
+    for (i, digit) in digits.enumerated() {
+        if digit == digits[(i + 1) % n] {
+            sum += digit
+        }
+    }
+    print(sum)
+}
+
+func part2(_ digits: [Int]) {
+    let n = digits.count
+    var sum = 0
+    for (i, digit) in digits.enumerated() {
+        if digit == digits[(i + n / 2) % n] {
+            sum += digit
+        }
+    }
+    print(sum)
+}
+
+func main() {
+    let input = try? String(contentsOfFile: "input-01.txt", encoding: .ascii)
+    let digits = Array(input!).map { Int(String($0))! }
+    part1(digits)
+    part2(digits)
+}
+
+main()
